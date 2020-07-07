b32807afaf1a901d0e21688d3b0e125fdbc7466b
Julien Lamalle <eparis@atlassian.com>
2020-07-07 20:27:37 +0200

    I Change my exercices number 20

--- exo_20.rb
+++ exo_20.rb
@@ -1,8 +1,16 @@
-n = 1 
 
-puts "Choisis un nombre entre 1 et 25 "
+puts "Choisissez un nombre entre 1 et 25:"
 number = gets.chomp.to_i
-for n in (1..number)
-  puts ("# " * n) 
-  n += 1 
+i = 1
+y = number
+while i <= number
+  y.times do
+    print " "
+  end
+  i.times do
+    print "#"
+  end
+puts ""
+y -= 1
+i += 1
 end
